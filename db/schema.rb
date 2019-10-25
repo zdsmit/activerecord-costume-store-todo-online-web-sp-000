91bbf3c6581dc33965d290c17b1166241ef3ef8c
Zach  Smith <zdsmith001@gmail.com>
2019-10-25 20:03:25 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 1) do
+
+  create_table "costumes", force: :cascade do |t|
+    t.text "name"
+    t.float "price"
+    t.integer "size"
+    t.string "image_url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
