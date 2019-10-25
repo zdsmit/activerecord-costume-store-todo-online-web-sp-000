753b3306742aed5542e649b2f5f0506e02dd5f30
Zach  Smith <zdsmith001@gmail.com>
2019-10-25 20:06:44 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,16 @@
-# Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+
+  def change
+    create_table :haunted_houses do |house|
+      house.string :name
+      house.string :location
+      house.string :theme
+      house.float :price
+      house.boolean :family_friendly
+      house.datetime :opening_date
+      house.datetime :closing_date
+      house.string :long_description
+    end
+  end
+  
+end
