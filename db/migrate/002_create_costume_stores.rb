2815be863754c5d223274e28b524bb16bf59022a
Zach  Smith <zdsmith001@gmail.com>
2019-10-25 20:07:58 +0000

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,13 +1,13 @@
 # Create your costume_stores migration here
-class CreateCostumeStore < ActiveRecord::Migration[4.2]
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
 
   def change
     create_table :costume_stores do |store|
       store.string :name
       store.string :location
       store.integer :costume_inventory
-      store.integer :number_of_employees
-      store.boolean :still_in_business?
+      store.integer :num_of_employees
+      store.boolean :still_in_business
       store.datetime :opening_time
       store.datetime :closing_time
     end
